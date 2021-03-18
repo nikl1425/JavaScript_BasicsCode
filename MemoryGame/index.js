91f3f70f3d03ad22a6c9e09cd57985105b35753f
Niklas Hjort <niklas@astne.dk>
2021-03-18 13:13:26 +0100

    MEMORYGAME DONE

--- MemoryGame/index.js
+++ MemoryGame/index.js
@@ -1,15 +1,27 @@
 var container = document.getElementById("container");
+var upperContainer = document.getElementById("foundHeading")
 container.style.width = "600px";
-container.style.height = "405px";
+container.style.height = "400px";
 container.style.position ="relative"
 container.style.border = "1px solid #000"
+container.style.whiteSpace = "nowrap"
 var numberOfImages = 6;
 let animalList = []
-var flipped = 0;
+var currentFlipCount = 0
+var maxFlipped = 2
+let girafPair = []
+let pantherPair = []
+let elephantPair = []
+winPantherTricker = false;
+winGirafTricker = false;
+winElephantTricker = false;
+var girafFound = document.createElement("h1")
+var pantherFound = document.createElement("h1")
+var elephantFound = document.createElement("h1")
 
 function SquareImg() {
     var squareImg = document.createElement('div')
-    squareImg.style = "width: 200px; height: 200px; background-color: blue; position: relative; border: 1px solid #000;"
+    squareImg.style = "width: 200px; height: 200px; background-color: blue; position: relative; border: 1px solid #000; box-sizing:border-box;float: left;"
     return (
         squareImg
     )
@@ -53,12 +65,12 @@ function ElephantImg() {
 function SquareImageSet(callbackA, callbackB) {
     let i = callbackA(), 
         s = callbackB(),
-        count = 0;
+        flipped = false;
     
     var object = {
         picture: i,
         square: s,
-        count: count
+        flipped: flipped
     }
     
     object.square.onclick = object.count + 1;
@@ -80,13 +92,20 @@ function CreateListOfSets(callback){
 
     var listOfAnimal = []
     listOfAnimal.push(girafSetOne, girafSetTwo, pantherSetOne, pantherSetTwo, ElephantSetOne, ElephantSetTwo)
+    
+    idCounter = 0
+
+    listOfAnimal.forEach(element => {
+        element.id = idCounter
+        idCounter += 1
+    });
     return(
         animalList = shuffle(listOfAnimal)
     )
 }
 
 CreateListOfSets(SquareImageSet)
-console.log(animalList)
+//console.log(animalList)
 
 
 function shuffle(originalArray) {
@@ -109,13 +128,161 @@ function shuffle(originalArray) {
     return array;
   }
 
-  function Game(){
-      animalList.forEach(element => {
-          element.picture.onclick = function() {console.log(this)}
-          
-          container.appendChild(element.picture)
-          console.log(element)
-      });
+
+  function checkWin(element){
+      // _------------GIRAF--------------
+     if (element.id == 1 || element.id == 0){
+         if(element.flipped == true){
+            girafPair.push(element)
+         }
+         if(element.flipped == false){
+            girafPair.pop()
+         }
+     }
+     
+ 
+     
+     if(girafPair.length >= 2){
+         girafFound.innerHTML = "Found the girafs"
+         girafFound.id = "girafHeading"
+         if (winGirafTricker == false){
+            console.log("tricker")
+            currentFlipCount -= 2
+            winGirafTricker = true
+           
+        }
+         
+     }
+     if(girafPair.length != 2){
+         girafFound.innerHTML = ""
+     }
+     upperContainer.appendChild(girafFound)
+    // _------------PANTHER--------------
+    if (element.id == 2 || element.id == 3){
+        if(element.flipped == true){
+           pantherPair.push(element)
+        }
+        if(element.flipped == false){
+           pantherPair.pop()
+        }
+    }
+    
+   
+    
+    if(pantherPair.length >= 2){
+        pantherFound.innerHTML = "Found the Panthers"
+        pantherFound.id = "girafHeading"
+        if (winPantherTricker == false){
+            console.log("tricker")
+            currentFlipCount -= 2
+            winPantherTricker = true
+           
+        }
+        
+        
+    }
+    if(pantherPair.length != 2){
+        pantherFound.innerHTML = ""
+    }
+    upperContainer.appendChild(pantherFound)
+    // _------------ELEPHANT--------------
+    if (element.id == 4 || element.id == 5){
+        if(element.flipped == true){
+           elephantPair.push(element)
+           
+        }
+        if(element.flipped == false){
+           elephantPair.pop()
+        }
+    }
+    
+  
+    
+    if(elephantPair.length >= 2){
+        elephantFound.innerHTML = "Found the Elephants"
+        elephantFound.id = "girafHeading"
+        if (winElephantTricker == false){
+            console.log("tricker")
+            currentFlipCount -= 2
+            winElephantTricker = true
+            console.log(winElephantTricker)
+            console.log(currentFlipCount)
+        }
+    }
+    if(elephantPair.length != 2){
+        elephantFound.innerHTML = ""
+    }
+    upperContainer.appendChild(elephantFound)
+
+    };
+
+
+
+  function Game(callback, callbackTwo){
+      id_counter = 0
+      
+      animalList.forEach(element => { 
+        var innerContainer = document.createElement("div")
+        innerContainer.style.width = "200px"
+        innerContainer.style.height = "200px"
+        innerContainer.style = "box-sizing:border-box;float: left;"
+        element.picture.style.display = "none"
+        innerContainer.appendChild(element.square)
+        innerContainer.appendChild(element.picture)
+        
+          element.square.onclick = function() {
+            if(currentFlipCount < maxFlipped){
+              element.flipped = true;
+              currentFlipCount += 1
+              console.log(currentFlipCount)
+              callback(element)
+              
+              //console.log(element)
+              //console.log(currentFlipCount)
+            if (element.flipped === false){
+                element.square.style.display = "block"
+                element.picture.style.display = "none"
+
+                
+
+                // CHECKING WIN CONDITION
+                if (animalList[0].id == 1 && animalList[0].flipped == true){
+                    console.log("win")
+                }
+            }
+            else {
+                element.picture.style.display = "block"
+                element.square.style.display = "none"
+            }
+            }
+        }
+        
+        element.picture.onclick = function(){
+            currentFlipCount -= 1
+            element.flipped = false;
+            callback(element)
+            if(element === true){
+                element.square.style.display = "none"
+                element.picture.style.display = "block"
+                
+            }
+            else {
+                element.picture.style.display = "none"
+                element.square.style.display = "block"
+            }
+        }
+        container.appendChild(innerContainer)
+        console.log(innerContainer)
+      });  
   }
 
-  Game()
+  Game(checkWin)
+
+  function checkWinTwo(){
+      console.log("hi")
+      console.log(pantherPair)
+      if (pantherPair.length == 2){
+        console.log("currentFlipCount")
+        currentFlipCount -= 2          
+      }
+  }
